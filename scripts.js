8b1bc3522215a3e372087e1b17d220ffa71cfc5d
Gusthavo Rangel <72213417+GusthavoRV@users.noreply.github.com>
2023-12-01 00:08:08 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,31 +1,11 @@
-
 document.querySelector('.letter').addEventListener('click', () => {
-    document.querySelector('.paper').style.opacity = 1;
-    document.querySelector('.paper').style.visibility = 'visible';
+    const paper = document.querySelector('.paper');
+    const spotifyWidget = document.querySelector('.spotify-widget iframe');
+
+    // Toggle a visibilidade da carta
+    paper.style.opacity = (paper.style.opacity === '0' || paper.style.opacity === '') ? '1' : '0';
+    paper.style.visibility = (paper.style.visibility === 'hidden' || paper.style.visibility === '') ? 'visible' : 'hidden';
+
+    // Toggle a visibilidade do widget do Spotify
+    spotifyWidget.style.display = (spotifyWidget.style.display === 'none' || spotifyWidget.style.display === '') ? 'block' : 'none';
 });
-
-// Função para enviar e-mail
-const enviarEmail = () => {
-    // Substitua 'your_service_id' e 'your_template_id' pelos valores fornecidos pelo EmailJS após o registro
-    const serviceID = 'service_fuvkio9';
-    const templateID = 'template_snjqjom';
-    const publicKeyCredential = 'EnsXAQ5cnAjaaqdcG'
-
-    // Substitua os valores do e-mail conforme necessário
-    const emailParams = {
-        to_email: 'keetilyn.eduardaa@gmail.com', // substitua pelo endereço de e-mail do destinatário
-        subject: 'Assunto do E-mail',
-        text: 'Corpo do E-mail',
-    };
-
-    emailjs.send(serviceID, templateID, emailParams, publicKeyCredential)
-        .then((response) => {
-            console.log('E-mail enviado:', response);
-        })
-        .catch((error) => {
-            console.error('Erro ao enviar e-mail:', error);
-        });
-};
-
-// Chame a função enviarEmail quando o botão for clicado
-document.getElementById('acceptButton').addEventListener('click', enviarEmail);
